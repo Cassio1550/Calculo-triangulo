49d21f04d6745d0e37f541b5f7bef45eab7e6796
Cassio1550 <cassioops3@gmail.com>
2018-07-20 18:04:20 -0300

    v1.0

---
+++ Aula01/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Globalization;
+namespace Aula01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Triangulo X, Y;
+
+
+            double areaX, areaY, p;
+
+            X = new Triangulo();
+
+            Y = new Triangulo();
+
+            
+
+
+            X.a = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            X.b = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            X.c = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Y.a = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Y.b = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Y.c = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+
+            p = (X.a + X.b + X.c) / 2.0;
+            areaX = Math.Sqrt(p * (p - X.a) * (p - X.b) * (p - X.c));
+
+            p = (Y.a + Y.b + Y.c) / 2.0;
+            areaY = Math.Sqrt(p * (p - Y.a) * (p - Y.b) * (p - Y.c));
+
+            Console.WriteLine("AREA DO TRIANGULO X: " + areaX.ToString("F4", CultureInfo.InvariantCulture));
+            Console.WriteLine("AREA DO TRIANGULO Y: " + areaY.ToString("F4", CultureInfo.InvariantCulture));
+
+
+            if (areaX > areaY) {
+
+                Console.WriteLine("TRIANGULO DE MAIOR AREA: X");
+
+
+            } else if (areaY > areaX)
+
+            {
+
+                Console.WriteLine("TRIANGULO DE MAIOR AREA: Y");
+
+            } else
+
+            { Console.WriteLine("AREAS IGUAIS");
+
+
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Aula01/Triangulo.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Aula01
+{
+    class Triangulo
+    {
+        public double a, b, c;
+
+    }
+}
